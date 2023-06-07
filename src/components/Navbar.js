9d42dc9194107d5aa8f9641bb9fba289b30973e6
Skand Gaur <114177298+gitskand@users.noreply.github.com>
2023-06-07 17:00:53 +0530

    fortnite

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,52 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
+
+// export default function Navbar() {
+//   const [darkMode, setDarkMode] = useState(false);
+  
+ 
+
+//   const toggleDarkMode = () => {
+//     setDarkMode(!darkMode);
+//     document.body.classList.toggle('dark-mode');
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <nav className={`fixed-top navbar navbar-expand-lg navbar-${darkMode ? 'dark' : 'light'} ${darkMode ? 'bg-dark' : 'bg-primary'}`}>
+//           <div className="container-fluid">
+//             <a className="navbar-brand " style={{ color: "white" }} href="/">FortNite</a>
+//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                 <li className="nav-item">
+//                   <a className="nav-link" style={{ color: "white" }} aria-current="page" href="/">Home</a>
+//                 </li>
+//               </ul>
+
+//               <div className="form-check form-switch my-2">
+//                 <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={darkMode} onChange={toggleDarkMode} />
+//                 <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
+//               </div>
+             
+
+//             </div>
+//           </div>
+
+
+
+//         </nav>
+//       </div>
+//     </>
+//   );
+// }
+import React, { useState } from 'react';
 
 export default function Navbar() {
   const [darkMode, setDarkMode] = useState(false);
-  
- 
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -14,31 +56,48 @@ export default function Navbar() {
   return (
     <>
       <div>
-        <nav className={`navbar navbar-expand-lg navbar-${darkMode ? 'dark' : 'light'} ${darkMode ? 'bg-dark' : 'bg-primary'}`}>
+        <nav className={`fixed-top navbar navbar-expand-lg navbar-${darkMode ? 'dark' : 'light'} ${darkMode ? 'bg-dark' : 'bg-primary'}`}>
           <div className="container-fluid">
-            <a className="navbar-brand " style={{ color: "white" }} href="/">FortNite</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a className="navbar-brand" style={{ color: "white" }} href="/">
+              FortNite
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" style={{ color: "white" }} aria-current="page" href="/">Home</a>
+                  <a className="nav-link" style={{ color: "white" }} aria-current="page" href="/">
+                    Home
+                  </a>
                 </li>
               </ul>
 
               <div className="form-check form-switch my-2">
-                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={darkMode} onChange={toggleDarkMode} />
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault"
+                  checked={darkMode}
+                  onChange={toggleDarkMode}
+                />
                 <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
               </div>
-             
-
             </div>
           </div>
-
-
-
         </nav>
+      </div>
+
+      <div style={{ paddingTop: "75px" }}>
+        
       </div>
     </>
   );
